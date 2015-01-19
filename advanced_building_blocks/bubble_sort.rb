800a683f0b1c92fe723758328d709a0287cefde1
Hojoon Ji <sleeperbus@gmail.com>
2015-01-19 12:29:21 +0000

    ...

--- advanced_building_blocks/bubble_sort.rb
+++ advanced_building_blocks/bubble_sort.rb
@@ -1,1 +1,18 @@
 #bubble sort
+def bubble_sort(array)
+    array.each_with_index do |num, idx|
+        max = idx
+        for i in idx+1 .. array.size - 1
+            if array[idx] > array[i]
+                max = i
+            end
+        end
+        if max != idx
+            array[idx], array[max] = array[max], array[idx]
+        end
+        puts array.join(", ")
+    end
+    array
+end
+
+puts bubble_sort([4,3,78,2,0,2])

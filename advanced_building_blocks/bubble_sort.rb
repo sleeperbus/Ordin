73d073b25bde0b5205ed186feb27105669484b05
Hojoon Ji <sleeperbus@gmail.com>
2015-01-19 23:58:47 +0900

    C style

--- advanced_building_blocks/bubble_sort.rb
+++ advanced_building_blocks/bubble_sort.rb
@@ -1,13 +1,15 @@
 #bubble sort
 def bubble_sort(array)
-    for i in 0 .. (array.size - 1)
-        for j in 0 .. (array.size - 2)
-            if array[j] > array[j+1]
-                array[j], array[j+1] = array[j+1], array[j]
+    loop do 
+        swaped = false
+        (array.size - 1).times do |idx|
+            if array[idx] > array[idx+1]
+                array[idx], array[idx+1] = array[idx+1], array[idx]
+                swaped = true
             end
-        end 
+        end
+        break unless swaped
     end
-    array
 end
 
 puts bubble_sort([4,3,78,2,0,2])

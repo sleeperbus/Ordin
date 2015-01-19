51793a9aa1ba4fd773f1e6a03ccb8631a46e1929
Hojoon Ji <sleeperbus@gmail.com>
2015-01-19 23:14:05 +0000

    bubble sort complete

--- advanced_building_blocks/bubble_sort.rb
+++ advanced_building_blocks/bubble_sort.rb
@@ -3,7 +3,6 @@ def bubble_sort(array)
     loop do 
         swaped = false
         (array.size - 1).times do |idx|
-            puts "idx: #{idx}"
             if array[idx] > array[idx+1]
                 array[idx], array[idx+1] = array[idx+1], array[idx]
                 swaped = true
@@ -14,5 +13,24 @@ def bubble_sort(array)
     array
 end
 
-puts bubble_sort([4,3,78,2,0,2])
-puts bubble_sort([6, 5, 4, 3, 2, 1, 7, 8, 9, 10])
+def bubble_sort_by(array)
+    loop do 
+        swaped = false 
+        (array.size - 1).times do |idx|
+            if yield(array[idx], array[idx+1]) < 0
+                array[idx], array[idx+1] = array[idx+1], array[idx]
+                swaped = true 
+            end
+        end
+        break unless swaped
+    end
+    array
+end
+
+puts bubble_sort([4,3,78,2,0,2]).join(", ")
+puts bubble_sort([6, 5, 4, 3, 2, 1, 7, 8, 9, 10]).join(", ")
+
+x = bubble_sort_by(["hi", "hello", "hey"]) do |left, right|
+    right.length - left.length
+end
+puts x.join(" / ")

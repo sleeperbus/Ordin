237da9d90830fbf3649ebc72b303fa62c3d11e70
Hojoon Ji <sleeperbus@gmail.com>
2015-01-22 03:57:53 +0000

    lunch

--- beginning_ruby/chapter_6.rb
+++ beginning_ruby/chapter_6.rb
@@ -1,39 +1,67 @@
-class Square
-    def initialize
-        @@number_of_squares = defined?(@@number_of_squares) ? @@number_of_squares + 1 : 1
-    end 
+
+
+class Person
+    def initialize(name)
+        set_name(name)
+    end
     
-    def Square.count 
-        @@number_of_squares
+    def name 
+        @first_name + " " + @last_name
+    end
+
+    private    
+    def set_name(name)
+        first_name, last_name = name.split(/\s+/)
+        set_first_name(first_name)
+        set_last_name(last_name)
+    end
+    
+    def set_first_name(name)
+        @first_name = name 
+    end
+    
+    def set_last_name(name)
+        @last_name = name
     end
 end
 
 class Person
-    def initialize(name)
-        @name = name
+    def initialize(age)
+        @age = age
     end
     
-    def name 
-        @name
+    def age 
+        @age
     end
+    
+    def age_difference_with(other_person)
+        (self.age - other_person.age).abs
+    end
+    
+    protected :age
 end
 
-class Doctor < Person
-    def name 
-        "Dr." + super
-    end
+class Animal
+    attr_accessor :name 
+    
+    def initialize(name)
+        @name = name
+    end  
 end
 
-
-# overriding
-class Dog
+class Cat < Animal
     def talk
-        puts "Woof!"
+        "Meaow!" 
     end
 end
 
-class Dog 
+class Dog < Animal
     def talk
-        puts "Howl!"
+        "Woof!"
     end
 end
+
+#animals = [Cat.new("Flossie"), Dog.new("Clive"), Cat.new("Max")]
+#animals.each do |animal|
+#    puts animal.talk
+#end

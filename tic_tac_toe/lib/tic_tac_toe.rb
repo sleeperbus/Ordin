ed529fc300cc2cc0d1eebe3694cacc623ab38aa3
Hojoon Ji <sleeperbus@gmail.com>
2015-01-28 22:14:56 +0900

    ...

--- tic_tac_toe/lib/tic_tac_toe.rb
+++ tic_tac_toe/lib/tic_tac_toe.rb
@@ -1,6 +1,7 @@
 require "tic_tac_toe/version"
 require_relative "./tic_tac_toe/cell.rb"
 require_relative "./tic_tac_toe/player.rb"
+require_relative "./tic_tac_toe/board.rb"
 
 module TicTacToe
   # Your code goes here...
---
+++ tic_tac_toe/spec/board_spec.rb
@@ -0,0 +1,16 @@
+require "spec_helper"
+
+module TicTacToe
+	describe Board do 
+		context "#initialize" do
+			it "initializes the board with a grid" do 
+				expect { Board.new(grid: "grid") }.to_not raise_error
+			end
+
+			it "sets the grid with three rows by default" do 
+				board = Board.new
+				expect (board.grid).to have(3).things
+			end
+		end
+	end
+end

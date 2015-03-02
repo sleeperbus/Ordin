ecd6bbd032bb2127c227ddae42f56bec6cd1d4cf
Hojoon Ji <sleeperbus@gmail.com>
2015-03-02 09:28:57 +0000

    ..

--- test/recursion.rb
+++ test/recursion.rb
@@ -24,12 +24,42 @@ roman_mapping = {
   1 => "I"
 }
 
-def to_roman(roman_mapping, num)
-  result = ""
-  roman_mapping.each do |key, value|
-    div, mod = num.divmod(key)  
-    return if mod == 0
-    result += value*div 
-    to_roman(roman_mapping, mod)
+def to_roman(roman_mapping, num, result = "")
+  return result if num == 0
+  roman_mapping.keys.each do |divisor|
+    quotinent, modulus = num.divmod(divisor)
+    result << roman_mapping[divisor] * quotinent
+    return to_roman(roman_mapping, modulus, result) if quotinent > 0
   end
 end
+
+puts to_roman(roman_mapping, 11343, "")
+
+
+int_mapping = {
+  "M" => 1000,
+  "CM" => 900,
+  "D" => 500,
+  "CD" => 400,
+  "C" => 100,
+  "XC" => 90,
+  "L" => 50,
+  "XL" => 40,
+  "X" => 10,
+  "IX" => 9,
+  "V" => 5,
+  "IV" => 4,
+  "I" => 1
+}
+def to_int(int_mapping, roman, result = 0)
+  return result if roman.empty? 
+  int_mapping.keys.each do |key|
+    if roman.start_with? key 
+      result += int_mapping[key] 
+      roman.slice!(0, key.length)
+      return to_int(int_mapping, roman, result)
+    end
+  end 
+end
+
+puts to_int(int_mapping, "MMCMDCD", 0)
